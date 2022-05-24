1826742a7c876c5a30fbbefa75e8fcef4d1ae059
Patrick Henninger <privat@skyraptor.eu>
2022-05-24 23:09:50 +0200

    ignore
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

--- test/test.ts
+++ test/test.ts
@@ -10,13 +10,16 @@ describe('Logger', function() {
   it('should log to the console', function() {
     Logger.setVerboseness('Test', 4)
     Logger.verbose('Test', 3, 'Hello, World!')
+    // @ts-ignore
     expect( console.log.calledOnce ).to.be.true
+    // @ts-ignore
     expect( console.log.calledWith('[Test][3] Hello, World!') ).to.be.true;
   });
 
   it('should log not to the console if verbosity is too low', function() {
     Logger.setVerboseness('Test', 1)
     Logger.verbose('Test', 3, 'Hello, World!')
+    // @ts-ignore
     expect( console.log.calledOnce ).to.be.false
   });
 });
