9d7120313dee75dfeaa7538dc605b663e3340507
Patrick Henninger <privat@skyraptor.eu>
2022-05-25 13:56:07 +0200

    GTS lint & fix working
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,52 @@
-import chalk, { ChalkInstance, Color } from 'chalk'
+import chalk, { ChalkInstance, Color } from 'chalk';
 
 declare type VerbosityMap = {
-    [key: string]: number
-}
+  [key: string]: number;
+};
 
 declare type ColorMap = {
-    [key: string]: ChalkInstance|Color
-}
+  [key: string]: ChalkInstance | Color;
+};
 
 class Logger {
-    verboseness: VerbosityMap = {}
-    colors: ColorMap = {}
+  verboseness: VerbosityMap = {};
+  colors: ColorMap = {};
 
-    verbose(module: string, verboseness: number, message: string, ...extras: any[]) {
-        let colorFunction: ChalkInstance = chalk.white
-        if (module in this.colors) {
-            if (typeof this.colors[module] === 'string' && this.colors[module] as string in chalk) {
-                colorFunction = chalk[this.colors[module] as Color as keyof ChalkInstance] as ChalkInstance
-            } else if (typeof this.colors[module] === 'function') {
-                colorFunction = this.colors[module] as ChalkInstance
-            }
-        }
-
-        if ((this.verboseness[module] || 1) >= verboseness) {
-            console.log(`[${colorFunction(module)}][${verboseness}] ${message}`, ...extras)
-        }
+  verbose(
+    module: string,
+    verboseness: number,
+    message: string,
+    ...extras: string[]
+  ) {
+    let colorFunction: ChalkInstance = chalk.white;
+    if (module in this.colors) {
+      if (
+        typeof this.colors[module] === 'string' &&
+        (this.colors[module] as string) in chalk
+      ) {
+        colorFunction = chalk[
+          this.colors[module] as Color as keyof ChalkInstance
+        ] as ChalkInstance;
+      } else if (typeof this.colors[module] === 'function') {
+        colorFunction = this.colors[module] as ChalkInstance;
+      }
     }
 
-    setVerboseness(module: string, verboseness: number) {
-        this.verboseness[module] = verboseness
+    if ((this.verboseness[module] || 1) >= verboseness) {
+      console.log(
+        `[${colorFunction(module)}][${verboseness}] ${message}`,
+        ...extras
+      );
     }
+  }
 
-    setColor(module: string, color: ChalkInstance|Color) {
-        this.colors[module] = color
-    }
+  setVerboseness(module: string, verboseness: number) {
+    this.verboseness[module] = verboseness;
+  }
+
+  setColor(module: string, color: ChalkInstance | Color) {
+    this.colors[module] = color;
+  }
 }
 
-export default new Logger()
+export default new Logger();
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,24 +1,23 @@
-import { jest } from '@jest/globals'
-import Logger from '../src/index'
+import { jest } from '@jest/globals';
+import Logger from '../src/index';
 
 test('should log not to the console if verbosity is too low', () => {
-  const mock = jest.fn()
-  console.log = mock
+  const mock = jest.fn();
+  console.log = mock;
 
-  Logger.setVerboseness('Test', 4)
-  Logger.verbose('Test', 3, 'Hello, World!')
+  Logger.setVerboseness('Test', 4);
+  Logger.verbose('Test', 3, 'Hello, World!');
 
-  expect(mock.mock.calls.length).toBeTruthy()
-  expect(mock.mock.lastCall[0]).toBe('[Test][3] Hello, World!')
-})
-
+  expect(mock.mock.calls.length).toBeTruthy();
+  expect(mock.mock.lastCall[0]).toBe('[Test][3] Hello, World!');
+});
 
 test('should log not to the console if verbosity is too low', () => {
-  const mock = jest.fn()
-  console.log = mock
+  const mock = jest.fn();
+  console.log = mock;
 
-  Logger.setVerboseness('Test', 1)
-  Logger.verbose('Test', 3, 'Hello, World!')
+  Logger.setVerboseness('Test', 1);
+  Logger.verbose('Test', 3, 'Hello, World!');
 
-  expect(mock.mock.calls.length).toBeFalsy()
-})
+  expect(mock.mock.calls.length).toBeFalsy();
+});
