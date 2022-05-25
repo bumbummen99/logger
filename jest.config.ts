35f0c802dfb6b86355f6c27b60e8cac24c217f01
Patrick Henninger <privat@skyraptor.eu>
2022-05-25 19:58:22 +0200

    We are getting there
    Signed-off-by: Patrick Henninger <privat@skyraptor.eu>

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,16 @@ import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
 // Or async function
 export default async (): Promise<InitialOptionsTsJest> => {
   return {
-    preset: 'ts-jest/presets/js-with-ts-esm',
+    preset: 'ts-jest/presets/default-esm',
     verbose: true,
-    extensionsToTreatAsEsm: ['.ts']
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+      'ts-jest': {
+        useESM: true,
+      },
+    },
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    }
   };
 };
